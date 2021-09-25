a74579c8adfa56ff569bd7a3b97fb1512353c620
Lochlann Andrews <lochlanna@gmail.com>
2021-09-25 12:21:05 +1000

    Working of derive macro

--- producer/src/lib.rs
+++ producer/src/lib.rs
@@ -1,32 +1,7 @@
 
 mod blocking {
-    // use proc_macro::TokenStream;
-    // use quote::quote;
-    // use syn;
-
-    // fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
-    //     let name = &ast.ident;
-    //     let gen = quote! {
-    //         impl HelloMacro for #name {
-    //             fn hello_macro() {
-    //                 println!("Hello, Macro! My name is {}!", stringify!(#name));
-    //             }
-    //         }
-    //     };
-    //     gen.into()
-    // }
-    //
-    // #[proc_macro_derive(Producer)]
-    // pub fn derive_producer(_item: TokenStream) -> TokenStream {
-    //     // Construct a representation of Rust code as a syntax tree
-    //     // that we can manipulate
-    //     let ast = syn::parse(input).unwrap();
-    //     println!("The inside of the derived producer is {:?}", ast);
-    //     impl_hello_macro(&ast)
-    // }
-    //
-    //
-    // #[derive(Debug, Producer)]
+    use conductor_derive::Producer;
+    #[derive(Debug, Producer)]
     pub struct MyCoolDataStructure {
         name: String,
         id: usize
